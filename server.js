af1d0e586a1c45e747cd0bbf1ecfdfbe071fe122
Salman Tariq <salman@diligensoftware.com>
2017-09-22 21:52:44 -0400

    WIP

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ MongoClient.connect(url, function(err, db) {
   console.log("Connected successfully to mongo dbserver")
 
   app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/static/index.html')
+    res.sendFile('static/index.html' , { root : __dirname});
+    // res.sendFile(__dirname + '/static/index.html')
   })
 
   app.post('/submission', (req, res) => {
