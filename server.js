910492b7eb43b3529ffc1d313934dc2fc445b54f
Salman Tariq <salman@diligensoftware.com>
2017-09-22 21:58:04 -0400

    WIP

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 
